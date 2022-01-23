02f8f406d19673f1f86b45210962674f36508738
mousto16 <mbouombouomousto@gmail.com>
2022-01-23 15:05:06 +0100

    How to create module

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hospital Management",
+    'summary': "Hospital Management Mousto",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Moustapha",
+    'category': 'Productivity',
+    'description': """
+    This module is management of Hospital
+    """,
+    'sequence':-100,
+    'website': 'camersoftware.com',
+    'license': 'LGPL-3',
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'price': 104,
+    'currency': 'EUR'
+}

0e5ad80e55e3ea3293080c52856c66c038a0196e
mousto16 <mbouombouomousto@gmail.com>
2022-01-29 13:58:18 +0100

    add field tracking

--- models/patient.py
+++ models/patient.py
@@ -6,17 +6,18 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient "
     _inherit = ["mail.thread", 'mail.activity.mixin']
 
-    name = fields.Char(string='Name', required=True)
-    age = fields.Integer(string='Age')
+    name = fields.Char(string='Name', required=True, tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Other'),
-    ], required=True, default='male')
+    ], required=True, default='male', tracking=True)
     note = fields.Text(string='Description')
     """add to header in form view"""
     state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'),
-                              ('done', 'Done'), ('cancel', 'Cancelled')], default ='draft', string='Status')
+                              ('done', 'Done'), ('cancel', 'Cancelled')], default ='draft',
+                             string='Status', tracking=True)
 
     """define fonction to change the steps to the statusbar of header of view form"""
     def action_confirm(self):
